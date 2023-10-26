1691339c83347494d63f1551120a1747846ada6a
Javier Chavarri <javier.chavarri@gmail.com>
2023-10-26 09:38:57 +0000

    typo

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     rootDir: "./_build/default/",
     testMatch: ["**/*_test.js"],
-    // ignoring these, are they are just temporary copies from runtime_deps, the final ones are in
-    // the path with melange.emit target, e.g. _build/default/dom/test/test/dom/test/__snapshots__
+    /* Ignoring these, as they are just temporary copies from runtime_deps, the final ones are in the
+      path created using the melange.emit target, e.g. _build/default/dom/test/test/dom/test/__snapshots__ */
     modulePathIgnorePatterns: ["react/test/__snapshots__", "dom/test/__snapshots__"]
 };
   
